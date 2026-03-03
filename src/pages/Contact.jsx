86c74832a79030c2abb0872c37c474d54c968bf3
Gaurank Sharma <95328684+gaurank-sharma@users.noreply.github.com>
2026-03-03 17:54:28 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -234,7 +234,7 @@ const ContactUs = () => {
             
             {/* The SVG Map via Wikipedia (Grayscale Stylized via Tailwind filters) */}
             <img 
-              src="https://upload.wikimedia.org/wikipedia/commons/e/ee/India-map-en.svg" 
+              src="https://m.media-amazon.com/images/I/71DDgmWz-yL._SY522_.jpg" 
               alt="India Map" 
               className="w-full h-full object-contain filter invert opacity-10 brightness-50" 
             />
